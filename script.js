b951f6ed0578d1c5f472b1a99f45def13e6e5f62
Femi Efejuku <obito.femi@gmail.com>
2021-08-13 17:22:10 +0100

    made responsive

--- script.js
+++ script.js
@@ -5,9 +5,37 @@ new Splide( '.splide', {
     focus : 'center',
     gap: '20px',
 	pagination: false,
+	
+	
+} ).mount();
+
+var options = {
+	perPage: 3,
 	breakpoints: {
 		640: {
-			perPage: 2,
+			perPage: 1,
 		},
 	}
-} ).mount();
+};
+const modal = document.querySelector('.modal')
+const loginBtn = document.querySelector('.hamburger')
+const closeBtn = document.querySelector('.closeModal')
+
+loginBtn.addEventListener('click', openModal)
+closeBtn.addEventListener('click', closeModal)
+window.addEventListener('click', outsideClick )
+
+
+function openModal() {
+    modal.style.display ='block'
+}
+
+function closeModal() {
+    modal.style.display = 'none'
+}
+
+function outsideClick(e) {
+    if(e.target == modal) {
+        closeModal()
+    }
+}
