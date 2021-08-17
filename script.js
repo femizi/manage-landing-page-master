9f7f9e5abe32946e8cfd4f735cf7e5d955dc01b6
Femi Efejuku <obito.femi@gmail.com>
2021-08-17 15:11:56 +0100

    formatted properly

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
-new Splide( '.splide', {
-	type   : 'loop',
-	perPage: 3,
-	perMove: 1,
-    focus : 'center',
-    gap: '20px',
-	pagination: false,
-	
-	
-} ).mount();
+new Splide(".splide", {
+  type: "loop",
+  perPage: 3,
+  perMove: 1,
+  focus: "center",
+  gap: "20px",
+  pagination: false,
+}).mount();
 
 var options = {
-	perPage: 3,
-	breakpoints: {
-		640: {
-			perPage: 1,
-		},
-	}
+  perPage: 3,
+  breakpoints: {
+    640: {
+      perPage: 1,
+    },
+  },
 };
-const modal = document.querySelector('.modal')
-const loginBtn = document.querySelector('.hamburger')
-const closeBtn = document.querySelector('.closeModal')
+const modal = document.querySelector(".modal");
+const loginBtn = document.querySelector(".hamburger");
+const closeBtn = document.querySelector(".closeModal");
 
-loginBtn.addEventListener('click', openModal)
-closeBtn.addEventListener('click', closeModal)
-window.addEventListener('click', outsideClick )
-
+loginBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", outsideClick);
 
 function openModal() {
-    modal.style.display ='block'
+  modal.style.display = "block";
 }
 
 function closeModal() {
-    modal.style.display = 'none'
+  modal.style.display = "none";
 }
 
 function outsideClick(e) {
-    if(e.target == modal) {
-        closeModal()
-    }
+  if (e.target == modal) {
+    closeModal();
+  }
 }
